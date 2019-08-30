cbed3a98f5c1c64ca1b844ec66b50f704b80328d
Cesar Pinto <cesarpinto@MacBook-Air-de-Cesar.local>
2019-08-30 02:56:56 -0400

    fixed evaluator

--- node/evaluate.js
+++ node/evaluate.js
@@ -1,7 +1,13 @@
-const mochawesome = require("./mochawesome-report/mochawesome.json");
+const fs = require("fs");
 
 const evaluate = () => {
-  if (!mochawesome.stats.failures === 0) {
+  const mochawesome = JSON.parse(
+    fs.readFileSync("./mochawesome-report/mochawesome.json", {
+      encoding: "utf8"
+    })
+  );
+  console.log(mochawesome.stats.failures);
+  if (mochawesome.stats.failures === 0) {
     process.exit(0);
   } else {
     process.exit(1);
