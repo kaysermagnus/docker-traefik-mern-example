432f9dcfb88e35620470dcdcf29d0e436e74378f
Cesar Pinto <cesarpinto@MacBook-Air-de-Cesar.local>
2019-08-30 02:22:44 -0400

    added failure to test

--- node/tests/index.spec.ts
+++ node/tests/index.spec.ts
@@ -5,17 +5,21 @@ import { startExpressServer, startRestifyServer } from "../src/server";
 import { Server } from "http";
 
 describe("Server tests", () => {
+  let serve: Server;
+
+  afterEach(() => {
+    if (serve) serve.close();
+  });
+
   it("Express server should a Server instance", async () => {
     const ExpressServer = await startExpressServer();
-    const serve = ExpressServer.listen(8080);
-    expect(serve).to.be.instanceOf(Server);
-    serve.close();
+    serve = ExpressServer.listen(8080);
+    expect(serve).to.not.be.instanceOf(Server);
   });
 
   it("Restify server should a Server instance", async () => {
     const RestifyServer = await startRestifyServer();
-    const serve = RestifyServer.listen(8081);
+    serve = RestifyServer.listen(8081);
     expect(serve).to.be.instanceOf(Server);
-    serve.close();
   });
 });
